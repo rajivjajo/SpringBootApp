18e245a1ab30fc28cd7582bf0e800d5ea0e6e9c0
rajivjajo <rajivjajodia82552@gmail.com>
2023-04-08 04:00:30 +0530

    DisplayMsgOnWeb

---
+++ src/main/java/com/bridgelabz/spring/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello(){
+        return "Hello from RJ";
+    }
+}
